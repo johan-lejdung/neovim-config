3a835d3e7e6db1bfc46b0f5f90bf5599d37900cc
Johan Lejdung <johanlejdung@gmail.com>
2026-09-21 10:39:38 +0200

    feat: add <leader>yp to yank file path to clipboard

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,6 +18,12 @@ vim.keymap.set("n", "<leader>ts", function()
   vim.opt.list = not vim.opt.list:get()
 end, { desc = "[T]oggle whitespace [s]ymbols" })
 
+vim.keymap.set("n", "<leader>yp", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify(path)
+end, { desc = "[Y]ank file [p]ath to clipboard" })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
